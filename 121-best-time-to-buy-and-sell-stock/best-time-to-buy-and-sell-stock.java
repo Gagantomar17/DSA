967df538a39165c870a773672a4da165566ec5b6
Gagan Tomar <91053241+Gagantomar17@users.noreply.github.com>
2024-01-20 00:48:20 +0530

    Time: 2 ms (78.28%) | Memory: 61.9 MB (22.98%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,53 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE ;
-        int maxProfit = 0;
-        int curProfit = 0;
+        int min_price = prices[0];
+        int maxprof = 0;
 
-        for(int i=0 ; i<prices.length-1 ; i++){
-            if(prices[i] < minPrice){
-                minPrice = prices[i];
-            }
-            curProfit = prices[i+1] - minPrice;
-            if(curProfit > maxProfit){
-                maxProfit = curProfit ;
-            }
+        for(int i=1;i<prices.length;i++){
+            maxprof = Math.max(maxprof,prices[i]-min_price);
+            min_price = Math.min(prices[i],min_price);
         }
 
-        return maxProfit ;
+        return maxprof;
+        // int minBuy = prices[0] ;
+        // int maxSell = Integer.MIN_VALUE ;
+        // int l = 0, idx=0;
+        // int r = prices.length-1 ;
+        // if(prices.length<=1){
+        //     return 0;
+        // }
+        // while(idx < r){
+        //     if(prices[l] < minBuy){
+        //         minBuy = prices[l] ;
+        //         idx = l ;
+        //     }
+        //     if(prices[r] > maxSell){
+        //         maxSell = prices[r];
+        //     }
+        //     l++ ;
+        //     r--;
+        // }
+        // int profit = maxSell - minBuy ;
+        // if(profit <=0){
+        //     return 0;
+        // }
+        // return maxSell - minBuy ;
+
+        // int minPrice = Integer.MAX_VALUE ;
+        // int maxProfit = 0;
+        // int curProfit = 0;
+
+        // for(int i=0 ; i<prices.length-1 ; i++){
+        //     if(prices[i] < minPrice){
+        //         minPrice = prices[i];
+        //     }
+        //     curProfit = prices[i+1] - minPrice;
+        //     if(curProfit > maxProfit){
+        //         maxProfit = curProfit ;
+        //     }
+        // }
+
+        // return maxProfit ;
         
     }
 }
