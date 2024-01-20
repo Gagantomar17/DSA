be275831627b706fdae94c72b220fa0531a220cb
Gagan Tomar <91053241+Gagantomar17@users.noreply.github.com>
2024-01-21 00:34:13 +0530

    Time: 19 ms (32.86%) | Memory: 55.5 MB (69.46%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,26 +1,11 @@
-import java.util.Scanner ;
 class Solution {
-    static boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i=0 ; i<nums.length-1 ; i++){
             if(nums[i] == nums[i+1]){
                 return true ;
             }
         }
-        
         return false ;
-        
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter length of n array");
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        System.out.println("Enter "+n+" Elements ");
-        for(int i=0 ; i<n ; i++){
-            nums[i] = sc.nextInt();
-        }
-        System.out.println(containsDuplicate( nums));
-
     }
 }
