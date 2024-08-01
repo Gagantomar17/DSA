3e8f7992e2a53282c5b30752fd8e523a88866886
Gagan Tomar <91053241+Gagantomar17@users.noreply.github.com>
2024-08-01 16:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (9.09%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,35 +15,41 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0 ;
-        Stack<TreeNode> nodeStack = new Stack<>() ;
-        Stack<Integer> depthStack = new Stack<>() ;
-        
-        nodeStack.push(root);
-        depthStack.push(1);
-        int depth = 0 ;
-
-        while(!nodeStack.isEmpty()){
-            TreeNode current = nodeStack.pop();
-            int currDepth = depthStack.pop() ;
-            if(current !=null){
-                depth = (currDepth > depth)? currDepth : depth;
-
-                if(current.right != null){
-                    nodeStack.push(current.right);
-                    depthStack.push(currDepth + 1);
-                }
-
-                if(current.left != null){
-                    nodeStack.push(current.left);
-                    depthStack.push(currDepth + 1);
-                }
-
-
-
-            }
+        if(root == null){
+            return 0;
         }
-        return depth ;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left,right)+1;
+        // if(root == null) return 0 ;
+        // Stack<TreeNode> nodeStack = new Stack<>() ;
+        // Stack<Integer> depthStack = new Stack<>() ;
+        
+        // nodeStack.push(root);
+        // depthStack.push(1);
+        // int depth = 0 ;
+
+        // while(!nodeStack.isEmpty()){
+        //     TreeNode current = nodeStack.pop();
+        //     int currDepth = depthStack.pop() ;
+        //     if(current !=null){
+        //         depth = (currDepth > depth)? currDepth : depth;
+
+        //         if(current.right != null){
+        //             nodeStack.push(current.right);
+        //             depthStack.push(currDepth + 1);
+        //         }
+
+        //         if(current.left != null){
+        //             nodeStack.push(current.left);
+        //             depthStack.push(currDepth + 1);
+        //         }
+
+
+
+        //     }
+        // }
+        // return depth ;
         
     }
 }
